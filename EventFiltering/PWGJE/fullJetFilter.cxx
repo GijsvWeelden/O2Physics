2c65246b15bc07c78b76ebfa5cd3e6a59b3034b4
GijsvWeelden <55794847+GijsvWeelden@users.noreply.github.com>
2022-08-10 12:21:15 +0200

    Save point before O2Physics update

--- EventFiltering/PWGJE/fullJetFilter.cxx
+++ EventFiltering/PWGJE/fullJetFilter.cxx
@@ -40,16 +40,20 @@
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
+using selectedClusters = o2::soa::Filtered<o2::aod::EMCALClusters>;
 
-static const std::vector<std::string> matchInfoNames {"MinimumBias", "Patch", "MatchedJet"};
+static const std::vector<std::string> matchInfoNames {"Patch", "MatchedJet"}; //{"MB", "Patch", "MatchedJet"};
 
 struct fullJetFilter {
-  enum { kMatchedJet = 0,
-         kCategories };
+  enum { //kMinimumBias = 0,
+    kPatch = 0,
+    kMatchedJet,
+    kCategories };
 
   //event selection cuts
   Configurable<float> selectionHighPtTrack{"selectionHighPtTrack", 10., "Minimum track pT trigger threshold"};       //we want to keep all events having a track with pT above this
-  Configurable<float> selectionJetPt{"selectionJetPt", 25., "Minimum charged jet pT trigger threshold"}; //we want to keep all events having a charged jet with pT above this
+  Configurable<float> selectionJetPt{"selectionJetPt", 25., "Minimum full jet pT trigger threshold"}; //we want to keep all events having a charged jet with pT above this
+  Configurable<float> selectionHighECluster{"selectionHighPtCluster", 5., "Minimum cluster E trigger threshold"}; //we want to keep all events having a charged jet with pT above this
   Configurable<int> fMatchDist{"fMatchDist", 5, "Matching distance in number of towers"}; // Maximum clusters/patch distance
   // Acceptance cuts
   Configurable<float> cfgVertexCut{"cfgVertexCut", 10.0f, "Accepted z-vertex range"};
@@ -61,6 +65,9 @@ struct fullJetFilter {
   Configurable<float> cfgJetEtaCut{"cfgJetEtaCut", 0.7f, "Maximum (absolute) jet eta, dimensions of EMCal"};
   Configurable<float> cfgJetPhiMin{"cfgJetPhiMin", 1.396f, "Minimum jet phi, dimensions of EMCal"}; // 80 degrees
   Configurable<float> cfgJetPhiMax{"cfgJetPhiMax", 3.264f, "Maximum jet phi, dimensions of EMCal"}; // 187 degrees
+
+  Configurable<std::string> mClusterDefinition{"clusterDefinition", "kV3Default", "cluster definition to be selected, e.g. V3Default"};
+
   // Soft drop settings
   Configurable<float> cfg_zCut{"cfg_zCut", 0.1, "soft drop z cut"};
   Configurable<float> cfg_beta{"cfg_beta", 0.0, "soft drop beta"};
@@ -92,80 +99,29 @@ struct fullJetFilter {
     // Add histograms
     spectra.add("fCollZpos", "collision z position", HistType::kTH1F, {{200, -20., +20., "#it{z}_{vtx} position (cm)"}});
     spectra.add("fTrackPtSelected", "pT of selected high pT tracks", HistType::kTH1F, {{150, 0., +150., "track #it{p}_{T} (GeV/#it{c})"}});
-    spectra.add("fJetChPtSelected", "pT of selected high pT charged jets", HistType::kTH1F, {{150, 0., +150., "charged jet #it{p}_{T} (GeV/#it{c})"}});
-    //------------------------
-    // Minimum Bias histograms
-    //------------------------
-        spectra.add("fMBTrack", "MBTracks", HistType::kTH3F, {
-        {150, 0., +150., "track #it{p}_{T} (GeV/#it{c})"},
-        {fgkNPhiBins, kMinPhi, kMaxPhi, "#phi"},
-        {fgkNEtaBins, kMinEta, kMaxEta, "#eta"}
-        });
-    spectra.add("fMBJet", "MB Jet properties", HistType::kTH3F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{E} (GeV)"},
-        {fgkNPhiBins, kMinPhi, kMaxPhi, "#phi"},
-        {fgkNEtaBins, kMinEta, kMaxEta, "#eta"}
-        });
-    // Jet substructure
-    spectra.add("fMBJetZg", "MB Jet zg", HistType::kTH2F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{p}_{T}^{jet} (GeV)"},
-        {20, 0., 1., "#it{z}_{g}"}
-        });
-    spectra.add("fMBJetRg", "MB Jet Rg", HistType::kTH2F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{p}_{T}^{jet} (GeV)"},
-        {20, 0., 1., "#it{R}_{g}"}
-        });
-    spectra.add("fMBJetnsd", "MB Jet nsd", HistType::kTH2F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{p}_{T}^{jet} (GeV)"},
-        {50, 0, 50, "#it{n}_{SD}"}
-        });
-    //------------------------
-    // Selection histograms
-    //------------------------
-    spectra.add("fTrack", "Tracks", HistType::kTH3F, {
-        {150, 0., +150., "track #it{p}_{T} (GeV/#it{c})"},
-        {fgkNPhiBins, kMinPhi, kMaxPhi, "#phi"},
-        {fgkNEtaBins, kMinEta, kMaxEta, "#eta"}
-        });
-    spectra.add("fMaxPatch", "Max Patch", HistType::kTH3F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{E} (GeV)"},
-        {fgkNPhiBins, kMinPhi, kMaxPhi, "#phi"},
-        {fgkNEtaBins, kMinEta, kMaxEta, "#eta"}
-        }); // Max Patch info
+    spectra.add("fJetPtSelected", "pT of selected high pT jets", HistType::kTH1F, {{150, 0., +150., "jet #it{p}_{T} (GeV/#it{c})"}});
+    spectra.add("fClusterESelected", "E of selected high E clusters", HistType::kTH1F, {{150, 0., +150., "cluster #it{E} (GeV/#it{c})"}});
+    spectra.add("fClusterDistSelected", "Distance of selected  cluster to jet", HistType::kTH2F, {
+      {150, 0., +150., "cluster #it{E} (GeV/#it{c})"},
+      {100, 0., +10., "dist"}
+      });
+
+    spectra.add("fJetMB", "pT of MB jets", HistType::kTH1F, {{150, 0., +150., "jet #it{p}_{T} (GeV/#it{c})"}});
+    spectra.add("fClusterMB", "E of MB clusters", HistType::kTH1F, {{150, 0., +150., "cluster #it{E} (GeV/#it{c})"}});
+
     spectra.add("fCluster", "Cluster", HistType::kTH3F, {
         {fgkNPtBins, kMinPt, kMaxPt, "#it{E} (GeV)"},
         {fgkNPhiBins, kMinPhi, kMaxPhi, "#phi"},
         {fgkNEtaBins, kMinEta, kMaxEta, "#eta"}
         }); // Cluster info
     spectra.add("fJet", "Jet properties", HistType::kTH3F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{E} (GeV)"},
+        {fgkNPtBins, kMinPt, kMaxPt, "#it{p}_T (GeV)"},
         {fgkNPhiBins, kMinPhi, kMaxPhi, "#phi"},
         {fgkNEtaBins, kMinEta, kMaxEta, "#eta"}
         }); // Jet pt, eta, phi before matching
-    spectra.add("fMBJet", "MB Jet properties", HistType::kTH3F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{E} (GeV)"},
-        {fgkNPhiBins, kMinPhi, kMaxPhi, "#phi"},
-        {fgkNEtaBins, kMinEta, kMaxEta, "#eta"}
-        }); // Jet pt, eta, phi before matching
-    spectra.add("fJetTrigger", "Match info", HistType::kTH3F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{E}^{p} (GeV)"},
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{E}^{cl} (GeV)"},
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{p}_{T}^{jet} (GeV)"}
-        }); // Match info
-    spectra.add("fJetZg", "Jet zg", HistType::kTH2F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{p}_{T}^{jet} (GeV)"},
-        {20, 0., 1., "#it{z}_{g}"}
-        });
-    spectra.add("fJetRg", "Jet Rg", HistType::kTH2F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{p}_{T}^{jet} (GeV)"},
-        {20, 0., 1., "#it{R}_{g}"}
-        });
-    spectra.add("fJetnsd", "Jet nsd", HistType::kTH2F, {
-        {fgkNPtBins, kMinPt, kMaxPt, "#it{p}_{T}^{jet} (GeV)"},
-        {50, 0, 50, "#it{n}_{SD}"}
-        });
+
     // Tracks #MB events, #Patches, #Matched in one histogram
-    auto scalers{std::get<std::shared_ptr<TH1>>(spectra.add("fProcessedEvents", ";;Number of filtered events", HistType::kTH1F, {{kCategories, -0.5, kCategories + 0.5}}))};
+    auto scalers{std::get<std::shared_ptr<TH1>>(spectra.add("fProcessedEvents", ";;Number of filtered events", HistType::kTH1F, {{kCategories, -0.5, kCategories - 0.5}}))};
     for (uint32_t iS{1}; iS <= matchInfoNames.size(); ++iS) {
       scalers->GetXaxis()->SetBinLabel(iS, matchInfoNames[iS - 1].data());
     }
@@ -176,47 +132,91 @@ struct fullJetFilter {
   } // init()
 
   // Declare filters
+  o2::aod::EMCALClusterDefinition clusDef = o2::aod::emcalcluster::getClusterDefinitionFromString(mClusterDefinition.value);
+  Filter clusterDefinitionSelection = o2::aod::emcalcluster::definition == static_cast<int>(clusDef);
+
   Filter collisionFilter = nabs(aod::collision::posZ) < cfgVertexCut;
   // TODO: Should be fiducial jet cut. I.e.: eta_jet < eta_max - R
-  Filter jetEtaFilter = (nabs(aod::jet::eta) < cfgJetEtaCut);
-  Filter jetPhiFilter = ((aod::jet::phi < cfgJetPhiMax) && (aod::jet::phi > cfgJetPhiMin));
-  Filter trackFilter = (nabs(aod::track::eta) < cfgTrackEtaCut) && (aod::track::isGlobalTrack == (uint8_t) true) && (aod::track::pt > cfgTrackLowPtCut);
+  // Filter jetEtaFilter = (nabs(aod::jet::eta) < cfgJetEtaCut);
+  // Filter jetPhiFilter = ((aod::jet::phi < cfgJetPhiMax) && (aod::jet::phi > cfgJetPhiMin));
+  // Filter trackFilter = (nabs(aod::track::eta) < cfgTrackEtaCut) && (aod::track::isGlobalTrack == (uint8_t) true) && (aod::track::pt > cfgTrackLowPtCut);
 
   //using TrackCandidates = soa::Filtered<soa::Join<aod::Tracks, aod::TrackSelection>>;
 
   void process(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& collision,
-               //TrackCandidates const& tracks,
-               aod::Jets const& jets
-              //  aod::Tracks const& tracks,
-              //  aod::JetTrackConstituents const& constituents,
-              //  aod::JetConstituentsSub const& constituentsSub
+               aod::Jets const& jets,
+               aod::JetClusterConstituents const& clusterConstituents,
+               selectedClusters const& clusters
               )
   {
+    // TODO:
+    // Check if EMCAL trigger is fired. Skip otherwise.
+    // -> Filter on collision table?
+    // Construct patch proxy from cells
+    // -> Maybe do this in a different task?
+    // Match cluster with cells
+    // Set flags
     // collision process loop
     bool keepEvent[kCategories]{false};
-
-    // TODO: Check that there is an emcal trigger for this event
-    // Can we implement this as a filter on the event table?
-
-    // selectedClusters const& clusters
-    // for (const auto& cluster : clusters) {
-    for (auto& jet : jets)
-    {
-        if (jet.pt() > selectionJetPt){
-          spectra.fill(HIST("fJet"), jet.pt());
-          keepEvent[kMatchedJet] = true;
-          break;
-        }
+    int nMC = 0; // number of matched clusters
+    int nKC = 0; // number of kept clusters
+    // keepEvent[kMinimumBias] = true;
+    for (const auto& jet : jets){
+      spectra.fill(HIST("fJet"), 0., jet.phi(), jet.eta());
+      spectra.fill(HIST("fJetMB"), jet.pt());
+    }
+    for (const auto& cluster : clusters){
+      spectra.fill(HIST("fCluster"), cluster.energy(), cluster.phi(), cluster.eta());
+      spectra.fill(HIST("fClusterMB"), cluster.energy());
     }
 
-    //count events which passed the selections
-    for (int iDecision{0}; iDecision < kCategories; ++iDecision) {
+    for (auto clusterConstituent : clusterConstituents){
+      auto cluster = clusterConstituent.cluster();
+      // if (cluster.energy() > selectionHighECluster){
+        keepEvent[kPatch] = true;
+        spectra.fill(HIST("fClusterESelected"), cluster.energy());
+        break;
+      // }
+    }
+
+    for (const auto& cluster : clusters){
+      if (cluster.energy() > selectionHighECluster){
+        keepEvent[kPatch] = true;
+        spectra.fill(HIST("fClusterESelected"), cluster.energy());
+        nKC++;
+        break;
+      }
+    }
+
+    for (const auto& jet : jets){
+      for (const auto& cluster : clusters){
+        double dist = TMath::Sqrt(TMath::Power(cluster.phi() - jet.phi(), 2) + TMath::Power(cluster.eta() - jet.eta(), 2));
+        if (dist > jet.r()) continue;
+        if (cluster.energy() > selectionHighECluster){
+          keepEvent[kPatch] = true;
+          spectra.fill(HIST("fClusterDistSelected"), cluster.energy(), dist);
+          nKC++;
+          break;
+        }
+      }
+    }
+
+    for (const auto& jet : jets){
+      if (jet.pt() < selectionJetPt) continue;
+      if (TMath::Abs(jet.eta() > cfgJetEtaCut || jet.phi() < cfgJetPhiMin || jet.phi() > cfgJetPhiMax)) continue;
+      keepEvent[kMatchedJet] = true;
+      spectra.fill(HIST("fJetPtSelected"), jet.pt());
+      nMC++;
+    }
+
+    for (int iDecision{0}; iDecision < kCategories; iDecision++){
       if (keepEvent[iDecision]) {
         spectra.fill(HIST("fProcessedEvents"), iDecision);
       }
     }
+    // if (nKC - nMC != 0) std::cout << "nKC (" << nKC << ") - nMC (" << nMC << ") = " << nKC - nMC << std::endl;
 
-    tags(keepEvent[0]);
+    tags(keepEvent[0], keepEvent[1]);//, keepEvent[2]);
   } // process()
 };
 
